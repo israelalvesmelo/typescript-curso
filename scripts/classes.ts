039daa57323c4445f3a0f6e81e3a7ce82fd4513c
Israel Melo <israel.melo@ul.com>
2020-06-28 18:01:26 -0300

    Class 8 - Classes - part ||

--- scripts/classes.ts
+++ scripts/classes.ts
@@ -1,21 +1,37 @@
 class Person2 {
-    name: string;
+    private _name: string;
     constructor(name: string) {
-        this.name = name;
+        this._name = name;
     }
     print():void{
-        console.log(`name ${this.name}`);
+        console.log(`name ${this._name}`);
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
     }
 }
 class Employee2 extends Person2{
-    salary: number;
+    private _salary: number;
     constructor(name: string, salary: number) {
         super(name);
-        this.salary = salary;
+        this._salary = salary;
     }
     print() {
         super.print();
-        console.log(`Salary: ${this.salary}`)
+        console.log(`Salary: ${this._salary}`)
+    }
+
+    get salary(): number {
+        return this._salary;
+    }
+
+    set salary(value: number) {
+        this._salary = value;
     }
 }
 
@@ -25,7 +41,13 @@ p1.print();
 emp1.print();
 
 //That example isn't recomented
-let p2 = {name: "Name"} as Person2;
-p2.print()
-let p3 = <Person2>{};
-p3.print();
+// let p2 = {name: "Name"} as Person2;
+// p2.print()
+// let p3 = <Person2>{};
+// p3.print();
+
+//---------------------------
+let p4 = <Person2>{};
+let p5: Person2 = new Employee2("p5", 2000); //Polimorfismo
+p5.name;
+p5.print();
