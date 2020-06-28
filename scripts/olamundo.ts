c46552a81cb417d0b528e8020c0f5a7fa3d745d0
Israel Melo <israel.melo@ul.com>
2020-06-28 17:03:33 -0300

    Class 4 - Variables types on typeScript - part ||

--- scripts/olamundo.ts
+++ scripts/olamundo.ts
@@ -12,8 +12,8 @@ console.log(str3);
 
 function func():void {}
 
-if(bool != null){}
-if (bool != undefined){}
+if(bool !== null){}
+if (bool === undefined){}
 
 if (bool){
     let i : number;
@@ -35,3 +35,25 @@ console.log(tuple[1].valueOf()); // number
 enum Day {Monday = 1, Tuesday = 0, Sunday = 2}
 let day : Day = Day.Monday;
 console.log(`Today it's ${day} and tomorrow it's ${Day.Tuesday}`)
+
+// console.log("true == 1" , true == 1);
+// console.log("true === 1" , true === 1);
+// console.log("true !== 1", true !== 1);
+
+function add(v1: number, v2:number, v3?:number) : number { //variable it's not required
+    if(v3 !== undefined){
+      return v1 + v2 + v3;
+    }
+    return v1 + v2;
+}
+function addAny(v1: any, v2:any, v3?:any) : any { //variable it's not required
+    if(v3 !== undefined){
+      return v1 + v2 + v3;
+    }
+    return v1 + v2;
+}
+console.log(`Function addAny: ${this.addAny(2,4)}`);
+console.log(`Function addAny: ${this.addAny("2","4")}`);
+
+let exemple : void  = null;
+
