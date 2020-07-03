84aab80e19aea3815b266485f48b70576ddd1e2d
Israel Melo <israel.melo@ul.com>
2020-07-03 00:03:25 -0300

    Class  22 - Storage pt 01 - Session Storage

---
+++ scripts/sessionstorage.ts
@@ -0,0 +1,15 @@
+let key = "key1";
+let key2 = "key2";
+console.log(sessionStorage.getItem(key));
+console.log(sessionStorage.getItem(key2));
+sessionStorage.setItem(key, "valueKey1")
+sessionStorage.setItem(key2, "valueKey2")
+console.log(sessionStorage.getItem(key));
+console.log(sessionStorage.getItem(key2));
+
+for(let i=0; i< sessionStorage.length; i++){
+    console.log(sessionStorage.getItem(sessionStorage.key(i)));
+}
+
+//sessionStorage.removeItem(key); //To remove one attributes from session
+//sessionStorage.clear(); //To remove all attributes from session
