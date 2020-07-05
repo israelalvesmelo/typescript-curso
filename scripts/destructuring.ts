e79db57c0f91fd9fe6b2e9dd6a50b7b87f62def6
Israel Melo <israel.melo@ul.com>
2020-07-04 21:35:45 -0300

    Class 26 - Destructuring

---
+++ scripts/destructuring.ts
@@ -0,0 +1,31 @@
+let person = {name: "Israel", lastname: "Suane"};
+let name1 = person.name;
+let lastName1 = person.lastname;
+console.log(name1);
+console.log(lastName1);
+
+let {name: name2, lastname: lastname2} = person;
+
+console.log(name2);
+console.log(lastname2);
+
+let array =["DevDojo", "Awesomeness"];
+let [a, b] = array;
+
+console.log(a);
+console.log(b);
+
+class Person {
+    constructor(public name:string) {
+    }
+}
+function example(person: Person):void {
+    console.log(person.name);
+}
+function example2({name}):void {
+    console.log(name);
+}
+
+example(new Person("Willian"));
+
+example2({name: "Thor"});
